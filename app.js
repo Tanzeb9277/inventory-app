5905e075d5526ff718e7c61e4144562f305ec407
Tanzeb Choudhury <Tanzeem123456@gmail.com>
2022-06-05 20:41:35 -0400

    config fixes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = config.dbUrl;
+var mongoDB = process.env.MONGODB_URI || config.dbUrl;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
