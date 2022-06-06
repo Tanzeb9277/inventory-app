c42520c69399d1b6fcbc359ec8e7707bb036cc51
Tanzeb Choudhury <Tanzeem123456@gmail.com>
2022-06-05 20:37:47 -0400

    connecting to db

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var config = require('./config');
+const aws = require('aws-sdk');
+
+let config = new aws.S3({
+  dbUrl: process.env.DB_URL,
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
